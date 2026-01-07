57cc92ff3667235f24eea45c3b2f3f7230c5655e
Paweł Budek <pbudek1908@gmail.com>
2026-01-07 20:41:57 +0100

    Update SlipCreated Mailable to set "from" name as "Bet tracker" instead of "Mailgun Sandbox"

--- app/Mail/SlipCreated.php
+++ app/Mail/SlipCreated.php
@@ -25,7 +25,7 @@ class SlipCreated extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            from: new Address('postmaster@sandbox002cc7d1d7844d8bb29543754e76d477.mailgun.org', 'Mailgun Sandbox'),
+            from: new Address('postmaster@sandbox002cc7d1d7844d8bb29543754e76d477.mailgun.org', 'Bet tracker'),
             subject: '🎉Nowy kupon!',
         );
     }
